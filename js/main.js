a01633739ce7b07a1ee0195df10dc9df7260ee73
Sampob <sampo.bredenberg@metropolia.fi>
2021-03-02 14:52:49 +0200

    Merge branch 'sampob'
    2.3.2021

--- js/main.js
+++ js/main.js
@@ -53,7 +53,7 @@ function mainPage(json) {
         let a = document.createElement('a');
         a.className = 'productImg';
         a.id = String(num[i]);
-        a.href = '#product' + num[i];
+        a.href = './product.html#' + num[i];
 
         let img = document.createElement('img');
         //img.src = json[num[i]].imageSide;
@@ -81,6 +81,10 @@ function mainPage(json) {
         figure.appendChild(infoContainer);
         article.appendChild(figure);
 
+        article.addEventListener('click', function() {
+            console.log(num[i]);
+        });
+
         main.appendChild(article);
         //document.getElementById(String(num[i])).style.backgroundImage = "url('" + json[num[i]].imageMain + "')";
 
@@ -92,6 +96,8 @@ function mainPage(json) {
             document.getElementById(
                 String(num[i])).style.backgroundImage = "url('img/samBot.jpg')";
         }
+
+
     }
 
     /*
--- js/products.js
+++ js/products.js
@@ -8,7 +8,6 @@ const li = document.getElementsByTagName('li');
 
 for (let i = 0; i < li.length; i++) {
     li[i].addEventListener('click', function() {
-        console.log(i);
         updateProducts(jsonObj, i);
     });
 }
@@ -142,7 +141,7 @@ function updateProducts(json, num) {
             let a = document.createElement('a');
             a.className = 'productImg';
             a.id = String(i);
-            a.href = '#product' + i;
+            a.href = './product.html#' + i;
 
             let img = document.createElement('img');
             //img.src = json[num[i]].imageSide;
@@ -170,6 +169,10 @@ function updateProducts(json, num) {
             figure.appendChild(infoContainer);
             article.appendChild(figure);
 
+            article.addEventListener('click', function() {
+                console.log(i);
+            });
+
             main.appendChild(article);
             //document.getElementById(String(num[i])).style.backgroundImage = "url('" + json[num[i]].imageMain + "')";
 
@@ -190,5 +193,3 @@ function updateProducts(json, num) {
 }
 
 updateProducts(jsonObj, 0);
-
-console.log(jsonObj.length);
